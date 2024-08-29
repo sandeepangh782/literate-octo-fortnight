203a720d42d6009814f50c75dffd8b475fbebee6
notyashu <suryanshmehrotra110@gmail.com>
2024-08-29 10:45:33 +0530

    sync

--- beach/App.js
+++ beach/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import AppNavigator from './navigation/appNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
