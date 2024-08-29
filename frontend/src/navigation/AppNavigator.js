9ac0be44fa2ec3af3b156f716b4eb18fdc2028df
notyashu <suryanshmehrotra110@gmail.com>
2024-08-29 21:22:17 +0530

    added LoginScreen

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -1,14 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../screens/HomeScreen"; // Adjusted import path
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         {/* Add more screens here */}
       </Stack.Navigator>
     </NavigationContainer>
