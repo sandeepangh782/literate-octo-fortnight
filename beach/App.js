bf5ff6ac5841998f2a83a02221b9fc6575d52d94
notyashu <suryanshmehrotra110@gmail.com>
2024-08-29 19:24:23 +0530

    Blank Project new Start

--- beach/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AppNavigator from "./navigation/AppNavigator"; // Ensure the case matches exactly
-
-export default function App() {
-  return <AppNavigator />;
-}
