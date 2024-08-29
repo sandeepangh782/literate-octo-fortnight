0ecd5270361215a045a19135ad58a2603fc01acb
notyashu <suryanshmehrotra110@gmail.com>
2024-08-29 19:04:37 +0530

    sync

--- beach/navigation/AppNavigator.js
+++ beach/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from '../screens/LoginScreen';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "../screens/LoginScreen";
 // Import other screens as needed
 // import HomeScreen from '../screens/HomeScreen';
 
