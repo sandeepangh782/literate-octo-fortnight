67f0f8b5aae1cb796b59376bb8d8f773f1b7c496
notyashu <suryanshmehrotra110@gmail.com>
2024-08-29 22:15:18 +0530

    Sync

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import AppNavigator from "./src/navigation/AppNavigator"; // Adjusted import path
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
